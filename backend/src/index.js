6d41b3483cc8ccfaa1d644e879bc51d7958d6f35
lehai4 <86140878+lehai4@users.noreply.github.com>
2023-06-10 22:36:51 +0700

    config cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,10 +15,18 @@ dotenv.config();
 const app = express();
 const port = `${process.env.PORT}` || 3001;
 
+const whitelist = ["http://localhost:3000", "https://queuing-api.vercel.app"];
+
 const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true,
-  optionSuccessStatus: 200,
+  origin: (origin, callback) => {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  // credentials: true,
+  // optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 app.use(express.json());
